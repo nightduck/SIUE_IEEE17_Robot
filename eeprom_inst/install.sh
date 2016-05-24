5a8d2cea12283090823634ef80954bbb442eca72
root <root@client144-62.soe.siue.edu>
2016-05-24 21:20:50 +0000

    forgot the comments

--- eeprom_inst/install.sh
+++ eeprom_inst/install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+#fist copy the dtbo to the FW folder
 cp ../BB-PRUPID-MOTOR-00A0.dtbo /lib/firmware/
+
+#next write the eeprom on the board
 cat ./PRUMOTOR.data > /sys/bus/i2c/devices/1-0054/eeprom 
+
+#make sure that the dtbo script is executable
 chmod +x ./dtbo
+
+#place it in the initramfs-tools/hooks folder so it will be ran when doing the update script
 cp ./dtbo /etc/initramfs-tools/hooks/
-cp /boot/uboot/initrd.img-??????-bone?? /boot/uboot/initrd.img.bak
-/opt/scripts/tools/update_initrd.sh
+
+#make a backup of the bootloader just incase
+cp /boot/initrd.img-??????-bone?? /boot/initrd.img.bak
+
+#run the update script
+/opt/scripts/tools/developers/update_initrd.sh
